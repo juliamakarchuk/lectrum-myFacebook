26490c08822341650bd85ae2bffee757f4fda9e2
juliamakarchuk <juliamakarchuk366@gmail.com>
2018-11-20 13:30:01 +0200

    Updates

---
+++ precache-manifest.b42f7ec1f30a6d4221e14c9d621df617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0366208a184fb614759035f9508acd0",
+    "url": "/lectrum-myFacebook/static/media/lisa.c0366208.png"
+  },
+  {
+    "revision": "63ecd8f38ef06d51ebeb",
+    "url": "/lectrum-myFacebook/static/js/runtime~main.63ecd8f3.js"
+  },
+  {
+    "revision": "c0b4e26d849eec11a9b3",
+    "url": "/lectrum-myFacebook/static/js/main.c0b4e26d.chunk.js"
+  },
+  {
+    "revision": "ae4d89b0a84e233ce05a",
+    "url": "/lectrum-myFacebook/static/js/1.ae4d89b0.chunk.js"
+  },
+  {
+    "revision": "c0b4e26d849eec11a9b3",
+    "url": "/lectrum-myFacebook/static/css/main.694a4115.chunk.css"
+  },
+  {
+    "revision": "6475fbd74c77030192065e1b47331093",
+    "url": "/lectrum-myFacebook/index.html"
+  }
+];
